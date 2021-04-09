3e727603e13530f890d3e4d57cdbdc3691f6b98e
xiao <779740337@qq.com>
2021-04-09 14:08:37 +0800

    完成所有代码

--- day01.js
+++ day01.js
@@ -58,29 +58,29 @@
 //     ['123','-123','---10','0009','0','0000','2342.3','453.45','0123','123','0019'].map(item=>console.log(regex07(item)))
 // }
 //题目8：提取’‘nihao 2018-08-08 02:29:01 hhh’‘中完整的年月日和时间字段
-{
-    function regex08(item){
-        const year =  item.match(/\d{4}-\d{2}-\d{2}/)
-        const time = item.match(/\d{2}:\d{2}:\d{2}/)
-        return [year,time]
-    }
-
-    console.log(regex08("nihao 2018-08-08 02:29:01 hhh"));
-}
-//题目9：提取用js获得的时间的年月日和时间字段
-{
-    function getDate(){
-        const date = new Date().toString();
-        console.log(date); //Fri Apr 09 2021 13:35:26 GMT+0800
-        const year = date.match(/\s\d{4}\s/)[0]
-        const month = date.match(/[\w]+/g)[1]
-        const day = date.match(/\w+/)[0]
-        const time = date.match(/\d{2}:\d{2}:\d{2}/)[0]
-        return [year,month,day,time]
-    }
-
-    console.log(getDate());
-}
+// {
+//     function regex08(item){
+//         const year =  item.match(/\d{4}-\d{2}-\d{2}/)
+//         const time = item.match(/\d{2}:\d{2}:\d{2}/)
+//         return [year,time]
+//     }
+//
+//     console.log(regex08("nihao 2018-08-08 02:29:01 hhh"));
+// }
+// //题目9：提取用js获得的时间的年月日和时间字段
+// {
+//     function getDate(){
+//         const date = new Date().toString();
+//         console.log(date); //Fri Apr 09 2021 13:35:26 GMT+0800
+//         const year = date.match(/\s\d{4}\s/)[0]
+//         const month = date.match(/[\w]+/g)[1]
+//         const day = date.match(/\w+/)[0]
+//         const time = date.match(/\d{2}:\d{2}:\d{2}/)[0]
+//         return [year,month,day,time]
+//     }
+//
+//     console.log(getDate());
+// }
 //题目10：将以下网址提取出域名：
 //http://www.interoem.com/messageinfo.asp?id=35`
 // http://3995503.com/class/class09/news_show.asp?id=14
@@ -89,7 +89,7 @@
 // http://www.fincm.com/newslist.asp?id=415
 {
     function regex10(item){
-        return item.match("http://([\w]+)/")
+        return item.match("http://(.+)/")
     }
 
     console.log(regex10("http://www.fincm.com/newslist.asp?id=415"));
