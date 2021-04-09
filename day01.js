83d930901dfee2a2566e509d160b628a3c7388f2
xiao <779740337@qq.com>
2021-04-09 12:37:28 +0800

    完成第一题的抄袭

---
+++ day01.js
@@ -0,0 +1,9 @@
+//今天练习JS正则多项式的题目
+//题目一：写一个正则表达式，使其能同时识别下面所有的字符串：’bat’,’bit’, ‘but’, ‘hat’, ‘hit’, ‘hut’
+{
+    function regex01(){
+        //???
+    }
+    const arr = ['bat','bit','but','hat','hit','hut','mut']
+    arr.map(item=>console.log(regex01(item))) // 期待除了最后一个，全是true
+}
